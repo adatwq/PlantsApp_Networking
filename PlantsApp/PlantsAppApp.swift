06fb8df850cf0558c8b0266185d861b7d7406ed1
Khawlah Khalid <khawlah1920@hotmail.com>
2024-01-25 12:31:17 +0300

    Initial Commit

---
+++ PlantsApp/PlantsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsAppApp.swift
+//  PlantsApp
+//
+//  Created by Khawlah Khalid on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlantsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
